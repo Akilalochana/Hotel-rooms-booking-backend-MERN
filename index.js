a428efc366b7e22dcf720a8071a1b4506c709d10
Akila Lochana <168577881+Akilalochana@users.noreply.github.com>
2025-04-20 01:12:29 +0530

    add env file

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose';
 import userRouter from './routes/userRouter.js';
 import productRouter from './routes/productRouter.js';
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
 const app = express();
@@ -17,7 +20,7 @@ app.use((req, res, next)=>{
             token = token.replace("Bearer ","");
 
             jwt.verify(token, "ak-secret-19!",(err,decoded) =>{
-                  
+
                   if(!err){
                         req.user = decoded;
                   }
@@ -26,7 +29,7 @@ app.use((req, res, next)=>{
       next();
 })
 
-let mongoUrl = "mongodb+srv://admin:123@cluster0.hxocrso.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+let mongoUrl = process.env.MONGO_URL;
 
 mongoose.connect(mongoUrl);
 
